9caf30a0e33940cf9ea89224a5c41ccde8239c13
kkkwp <chxrryda@gmail.com>
2024-11-10 06:04:57 +0900

    feat: 스킬 부분 구현

---
+++ src/app/_components/Skills.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { motion } from 'framer-motion'
+import { skillsData } from '@/app/lib/data'
+import SectionHeading from '@/app/_components/SectionHeading'
+
+const fadeInAnimationsVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+}
+
+export default function Skills() {
+  return (
+    <section className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40">
+      <SectionHeading>Skills</SectionHeading>
+
+      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+        {skillsData.map((skill, index) => (
+          <motion.li
+            className="rounded-xl border border-black/[0.1] bg-white px-5 py-3"
+            key={index}
+            variants={fadeInAnimationsVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            custom={index}
+          >
+            {skill}
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import Header from '@/app/_components/Header'
 import Intro from '@/app/_components/Intro'
+import Skills from '@/app/_components/Skills'
+import Header from '@/app/_components/Header'
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Intro />
+      <main className="flex flex-col items-center px-4">
+        <Intro />
+        <Skills />
+      </main>
     </>
   )
 }
