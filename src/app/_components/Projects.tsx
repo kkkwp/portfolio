a0a046a02017fd066d8a40e75fa547097839e547
kkkwp <chxrryda@gmail.com>
2024-11-12 05:19:42 +0900

    fix: 프로젝트 영역 확장

--- src/app/_components/Projects.tsx
+++ src/app/_components/Projects.tsx
@@ -1,7 +1,9 @@
+import SectionHeading from '@/app/_components/SectionHeading'
+
 export default function Projects() {
   return (
-    <>
-      <section id="projects"></section>
-    </>
+    <section id="projects" className="mb-28 scroll-mt-28">
+      <SectionHeading>Projects</SectionHeading>
+    </section>
   )
 }
