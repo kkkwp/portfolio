f0bc783cfd8179cd4611c3acd4552d91a72e982b
kkkwp <chxrryda@gmail.com>
2024-11-14 16:59:37 +0900

    feat: 프로젝트 부분 구현 - 이미지는 mock 파일

--- src/app/_components/Projects.tsx
+++ src/app/_components/Projects.tsx
@@ -1,9 +1,61 @@
+import Image from 'next/image'
+import { FiArrowUpRight } from 'react-icons/fi'
+import { projectsData } from '@/app/lib/data'
 import SectionHeading from '@/app/_components/SectionHeading'
 
 export default function Projects() {
   return (
     <section id="projects" className="mb-28 scroll-mt-28">
       <SectionHeading>Projects</SectionHeading>
+      <div className="container">
+        <div className="mt-5 flex flex-col gap-20">
+          {projectsData.map((project) => (
+            <div
+              key={project.title}
+              className="relative z-0 overflow-hidden rounded-3xl bg-gray-800 px-8 pt-8 after:pointer-events-none after:absolute after:inset-0 after:-z-10 after:rounded-3xl after:outline after:outline-2 after:-outline-offset-2 after:outline-white/20 after:content-[''] md:px-10 md:pt-12 lg:px-20 lg:pt-16"
+            >
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div className="text-sm font-bold tracking-widest text-emerald-300">
+                    {project.date}
+                  </div>
+                  <h3 className="mt-2 text-2xl md:mt-5">{project.title}</h3>
+                  <hr className="my-4 border-t-2 border-white/5 md:my-5" />
+                  <span className="text-sm text-white/50 md:text-base">
+                    {project.description}
+                  </span>
+                  <div className="flex flex-col gap-4 md:flex-row lg:text-sm">
+                    <button className="mt-8 inline-flex h-12 w-full items-center justify-center gap-2 rounded-xl bg-emerald-300 px-8 font-semibold text-black md:w-auto">
+                      <span>상세 내역</span>
+                      <span>📖</span>
+                    </button>
+                    <a
+                      href={project.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <button className="mt-8 inline-flex h-12 w-full items-center justify-center gap-2 rounded-xl bg-white px-8 font-semibold text-gray-950 md:w-auto">
+                        <span>Visit Github</span>
+                        <FiArrowUpRight className="size-4" />
+                      </button>
+                    </a>
+                  </div>
+                </div>
+                <div className="relative">
+                  <Image
+                    // src={project.imageUrl}
+                    src="/assets/projects/dark-saas-landing-page.png"
+                    alt={project.title}
+                    width={800}
+                    height={507}
+                    className="-mb-4 mt-8 md:-mb-0 lg:absolute lg:mt-0 lg:h-full lg:w-auto lg:max-w-none"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   )
 }
